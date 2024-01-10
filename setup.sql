b57ba7f65056d277da20ba6850a9ffd9c0696d4e
rongshashiala <rongshashiala@gmail.com>
2024-01-11 00:17:46 +0800

    edit setup.sql

--- setup.sql
+++ setup.sql
@@ -18,3 +18,58 @@ LOAD DATA LOCAL INFILE 'Data/Stop.json'
         StopPositionLat = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.StopPositionLat')),
         StopAddress = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.StopAddress')),
         StationGroupID = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.StationGroupID'));
+
+
+CREATE TABLE IF NOT EXISTS Routes  (
+    RouteUID VARCHAR(20) PRIMARY KEY,
+    RouteName VARCHAR(255),
+    DepartureStopName VARCHAR(255),
+    DestinationStopName VARCHAR(255),
+    RouteMapImageUrl VARCHAR(255)
+);
+
+LOAD DATA LOCAL INFILE 'Data/Route.json' 
+    INTO TABLE Routes 
+    LINES TERMINATED BY '\n'
+    (@json)
+    SET
+        RouteUID = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.RouteUID')),
+        RouteName = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.RouteName')),
+        DepartureStopName = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.DepartureStopName')),
+        DestinationStopName = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.DestinationStopName')),
+        RouteMapImageUrl = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.RouteMapImageUrl'));
+
+CREATE TABLE IF NOT EXISTS Stations (
+    StationUID VARCHAR(20) PRIMARY KEY,
+    StationName VARCHAR(255),
+    StationGroupID VARCHAR(20),
+    StationPositionLon DOUBLE,
+    StationPositionLat DOUBLE
+);
+
+LOAD DATA LOCAL INFILE 'Data/Station.json' 
+    INTO TABLE Stations
+    LINES TERMINATED BY '\n'
+    (@json)
+    SET
+        StationUID = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.StationUID')),
+        StationName = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.StationName')),
+        StationGroupID = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.StationGroupID')),
+        StationPositionLon = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.StationPositionLon')),
+        StationPositionLat = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.StationPositionLat'));
+
+CREATE TABLE IF NOT EXISTS Stop_Of_Routes (
+    RouteUID VARCHAR(20),
+    RouteName VARCHAR(255),
+    Direction INT,
+    PRIMARY KEY (RouteUID, Direction)
+);
+
+LOAD DATA LOCAL INFILE 'Data/Stop_Of_Route.json' 
+    INTO TABLE Stop_Of_Routes
+    LINES TERMINATED BY '\n'
+    (@json)
+    SET
+        RouteUID = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.RouteUID')),
+        RouteName = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.RouteName')),
+        Direction = JSON_UNQUOTE(JSON_EXTRACT(@json, '$.Direction'));
